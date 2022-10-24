f5f997b813ce0b64324977364f3004b124fb834f
Anastasiia <anastasia@anastasia>
2022-10-24 11:15:27 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+import { Typography } from '@mui/material';
+
+import InputsForm from './componets/InputsForm'
+import PhoneConfirmForm from './componets/PhoneConfirmForm';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+return (
+  <div className='container'>
+    <Typography 
+      sx={{textAlign: 'center'}}
+      color={'DimGrey'} 
+      mb={5} 
+      variant='h4'
+    >
+        Регистрация
+    </Typography>
+    <InputsForm/>
+    {/* <PhoneConfirmForm/> */}
+  </div>
   );
 }
 
